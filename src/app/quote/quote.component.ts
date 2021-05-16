a8e84137060eca14cce62ffc6ef9bb681a6cc566
Tajeu2001 <tajeusanta@gmail.com>
2021-05-17 00:52:57 +0300

    create the quoteDetail function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,12 +13,12 @@ export class QuoteComponent implements OnInit {
     new Quote(1,'True confidence leaves no room for jealousy.','Nicki Minaj','Lukas William',0,0,new Date(2020,1,9)),
     new Quote(2,'Never regret anything that made you smile.','Angela Akoth','Gladwell Gee',0,0,new Date(2019,4,6)),
     new Quote(3,'Yesterday you said tomorrow. Just do it.','Ali Salim', 'Mary Magda',0,0,new Date(2020,5,7)),
-  ];/*
+  ];
 
   quoteDetails(index:number){
     this.quotes[index].showDetails = !this.quotes[index].showDetails;
   }
-
+/*
   completeQuote(isComplete:any, index:number){
     if (isComplete) {
       this.quotes.splice(index,1);
