d52d248f15f9031c6612e1c8fde42a1e80d5827e
Tajeu2001 <tajeusanta@gmail.com>
2021-05-17 01:08:29 +0300

    Create a submitquote function for submitting a new quote

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit,Output ,EventEmitter} from '@angular/core';
 import { Quote } from '../quote';
 
 
-/*@Component({
+@Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
@@ -16,7 +16,7 @@ export class QuoteFormComponent implements OnInit {
 this.addQuote.emit(this.newQuote);
   }
 
-
+/*
   constructor() { }
 
   ngOnInit(): void {
