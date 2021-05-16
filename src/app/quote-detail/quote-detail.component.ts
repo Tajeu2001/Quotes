7abdca5c92dcd0f547dd3e38d26c8e5bb7cab05a
Tajeu2001 <tajeusanta@gmail.com>
2021-05-17 00:48:02 +0300

    create a blueprint class

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
-
+/*
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit,Output ,EventEmitter} from '@angular/core';
 import { Quote } from '../quote';
 
 
-@Component({
+/*@Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
