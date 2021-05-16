030e57d772fb6b3e5933f858cf05804589bc9801
Tajeu2001 <tajeusanta@gmail.com>
2021-05-17 00:55:33 +0300

    create an addquote function for adding new quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -24,13 +24,13 @@ export class QuoteComponent implements OnInit {
       this.quotes.splice(index,1);
     }
   }
-/*
+
   addNewQuote(quote:any){
     let quoteLength = this.quotes.length;
     quote.id= quoteLength+1;
     quote.submitdate = new Date(quote.submitdate)
     this.quotes.push(quote)
-  }
+  }/*
 
   constructor() { }
 
